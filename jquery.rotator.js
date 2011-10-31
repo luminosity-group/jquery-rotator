b72a7a44d339fd30c9046b5695ce03f812218598
Eric J. Holmes <eric@ejholmes.net>
2011-10-31 15:47:04 -0700

    Added an option for stopping the animation of blur and restarting on focus to prevent the queued events bug.

--- jquery.rotator.js
+++ jquery.rotator.js
@@ -10,8 +10,12 @@
         var defaults = {
             interval: 4000,
             randomize: false,
-            fadetime: 500
+            fadetime: 500,
+            stopOnBlur: false
         };
+
+        /* Used when settings.stopOnBlur == true */
+        var stopped = false;
 
         /* Merge options with defaults */
         var settings = $.extend(true, {}, defaults, options);
@@ -32,17 +36,28 @@
             $('li', container).first().show().addClass('active');
         }
 
+        if (settings.stopOnBlur) {
+            $(window).blur(function(){
+                stopped = true;
+            }).focus(function(){
+                stopped = false;
+            });
+        }
+
         /* Does the fadein/fadeout to the next item in the list */
         function rotate() {
-            var current = $('li.active', container);
-            $('li', container).removeClass('active');
-            if ($(current).next().length > 0) {
-                $(current).fadeOut(settings.fadetime).next().fadeIn(settings.fadetime).addClass('active');
+            if (!settings.stopOnBlur || (settings.stopOnBlur && !stopped)) {
+                var current = $('li.active', container);
+                $('li', container).removeClass('active');
+                if ($(current).next().length > 0) {
+                    $(current).fadeOut(settings.fadetime).next().fadeIn(settings.fadetime).addClass('active');
+                }
+                else {
+                    $(current).fadeOut(settings.fadetime);
+                    $('li:first-child', container).fadeIn(settings.fadetime).addClass('active');
+                }
             }
-            else {
-                $(current).fadeOut(settings.fadetime);
-                $('li:first-child', container).fadeIn(settings.fadetime).addClass('active');
-            }
+
             setTimeout(rotate, settings.interval);
         }
 
